d4f6bbba421463a00fc4e692dfdceb964868301a
ttbeast10 <toma101006@gmail.com>
2025-06-18 16:28:30 +0300

    find_products_by_query() function answers on question 3

--- main.py
+++ main.py
@@ -19,11 +19,36 @@ def find_one_product(products_manager: ProductsManager) -> None:
     print("a random shirt with a price of 25 in the db:")
     print(products_manager.collection.find_one({"categories": "shirts", "price": 25}))
 
+def find_all_shirts(products_manager: ProductsManager) -> None:
+    print("getting all shirts:")
+    all_shirts_cursor = products_manager.collection.find({ "categories": "shirts"})
+    print(list(all_shirts_cursor))
+
+def find_products_by_query(products_manager: ProductsManager) -> None:
+    print("getting three proucts")
+    list_three_products = list(products_manager.collection.find(limit=3))
+    print(f"!!!!!!!!!\n{list_three_products}\n!!!!!!!\n")
+    print("getting all products with prices lower than 30:")
+    list_products_cheaper_than_30 = list(products_manager.collection.find({'price': {'$lt': 30}}))
+    print(f"!!!!!!!!!\n{list_products_cheaper_than_30}\n!!!!!!!\n")
+    print("prints all the pants cheaper with 80 < price < 150")
+    list_pants_in_price_range = list(products_manager.collection.find({'categories': 'pants','price': {'$lt': 150, '$gt': 80}}))
+    print(f"!!!!!!!!!\n{list_pants_in_price_range}\n!!!!!!!\n")
+    print("prints all the ties and suits")
+    list_pants_in_price_range = list(products_manager.collection.find({'$or': [{'categories': 'suits'}, {'categories': 'ties'}]}))
+    print(f"!!!!!!!!!\n{list_pants_in_price_range}\n!!!!!!!\n")
+    print("prints amount of products in the store")
+    count_products = products_manager.collection.count_documents({})
+    print(f"!!!!!!!!!\n{count_products}\n!!!!!!!\n")
+
+
+
 def main():
     products_manager = create_collection()
-    print("The brands we have are:")
-    print(products_manager.get_brand_names())
-    find_one_product(products_manager)
+    #print("The brands we have are:")
+    #print(products_manager.get_brand_names())
+    #find_one_product(products_manager)
+    find_products_by_query(products_manager)
 
 
 
