f494a8e8b4fd7e89b6b01f6025746035b9531bda
eitan <redscarf72@gmail.com>
2025-06-18 13:44:22 +0300

    made class more readable

--- course_shop.py
+++ course_shop.py
@@ -1,32 +1,29 @@
 import json
 from pymongo import MongoClient
-from typing import Set
+from typing import Set, Dict
+
 
 class ProductsManager:
-    def __init__(self) -> None:
+    def __init__(self, db_name: str, collection_name: str) -> None:
         self.client = MongoClient('127.0.0.1', 27017)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
 
-    def insert_json_to_collection(self, db_name: str, collection_name: str, json_data) -> None:
+    def insert_json_to_collection(self,json_data) -> None:
         """
         insert json data into the collection of the mongodb
 
-        :param db_name: database name
-        :param collection_name: collection name
         :param json_data: json data to add to the collection
         """
-        products_db = self.client[db_name]
-        inventory_collection = products_db[collection_name]
+        inventory_collection = self.collection
         inventory_collection.insert_many(json_data)
 
-    def get_brand_names(self, db_name: str, collection_name: str) -> Set[str]:
+    def get_brand_names(self) -> Set[str]:
         """
         gets all the brand name
 
-        :param db_name: database name
-        :param collection_name: collection name
         :return : set of different brands
         """
-        collection = self.client[db_name][collection_name]
-        all_products = list(collection.find())
+        all_products = list(self.collection.find())
         different_brands = set(product["brand"] for product in all_products)
         return different_brands
