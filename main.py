8a8be908b69f796c1e1d0cf2a949cd08240ee606
eitan <redscarf72@gmail.com>
2025-06-18 14:31:51 +0300

    saparated into functions

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from course_shop import ProductsManager
 DB_NAME = "Products"
 COLLECTION_NAME = "Inventory"
 
-
-def main():
-    products_manager = ProductsManager()
+def create_collection() -> ProductsManager:
+    products_manager = ProductsManager(DB_NAME, COLLECTION_NAME)
     with open("products.json") as products_file:
         products_file_data = json.load(products_file)
-    products_manager.insert_json_to_collection(DB_NAME, COLLECTION_NAME, products_file_data)
+    products_manager.insert_json_to_collection(products_file_data)
+    return products_manager
+
+def main():
+    products_manager = create_collection()
     print("The brands we have are:")
-    print(products_manager.get_brand_names(DB_NAME, COLLECTION_NAME))
+    print(products_manager.get_brand_names())
 
 
 if __name__ == '__main__':
