419750676de0b0a856cff74a2c969a35ac588496
eitan <redscarf72@gmail.com>
2025-06-18 14:22:46 +0300

    fixed error

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ DB_NAME = "Products"
 COLLECTION_NAME = "Inventory"
 
 def create_collection() -> ProductsManager:
-    products_manager = ProductsManager()
+    products_manager = ProductsManager(DB_NAME, COLLECTION_NAME)
     with open("products.json") as products_file:
         products_file_data = json.load(products_file)
-    products_manager.insert_json_to_collection(DB_NAME, COLLECTION_NAME, products_file_data)
+    products_manager.insert_json_to_collection(products_file_data)
     return products_manager
 
 def find_one_product(products_manager: ProductsManager) -> None:
@@ -20,7 +20,7 @@ def find_one_product(products_manager: ProductsManager) -> None:
 def main():
     products_manager = create_collection()
     print("The brands we have are:")
-    print(products_manager.get_brand_names(DB_NAME, COLLECTION_NAME))
+    print(products_manager.get_brand_names())
 
 
 
