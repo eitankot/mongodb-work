124ade4c6717c0649350fe44bec5e8863dac1190
eitan <redscarf72@gmail.com>
2025-06-18 16:43:29 +0300

    created count all products

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ COLLECTION_NAME = "Inventory"
 
 def create_collection() -> ProductsManager:
     products_manager = ProductsManager(DB_NAME, COLLECTION_NAME)
-    with open("products.json") as products_file:
-        products_file_data = json.load(products_file)
-    products_manager.insert_json_to_collection(products_file_data)
     return products_manager
 
 def main():
     products_manager = create_collection()
+    products_manager.client.drop_database(DB_NAME)
+    products_manager.insert_json_to_collection("products.json")
     # print("The brands we have are:")
     # print(products_manager.get_brand_names())
     print("get all categories")
