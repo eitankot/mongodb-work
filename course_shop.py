977aa189ee70aababb88017cd95f9c077c46dc12
hille <hilllel.benbaji@mail.huji.ac.il>
2025-06-19 13:12:12 +0300

    Create size counter and test main.

--- course_shop.py
+++ course_shop.py
@@ -1,6 +1,8 @@
 import json
+
+import pymongo.command_cursor
 from pymongo import MongoClient
-from typing import Set, Any
+from typing import Set, Any, List, Tuple, Iterator
 
 
 class ProductsManager:
@@ -20,42 +22,13 @@ class ProductsManager:
             json_file_data = json.load(products_file)
         inventory_collection.insert_many(json_file_data)
 
-    def get_brand_names(self) -> Set[str]:
+    def count_sizes(self) -> pymongo.command_cursor.CommandCursor:
         """
-        Test function which gets all the brands names.
-        :return : Set of the different brands.
+        Counts the total amount of each size, no matter which category.
+        :return: Iterator contains dictionaries, which have size id and total amount.
         """
-        all_products = list(self.collection.find())
-        different_brands = set(product["brand"] for product in all_products)
-        return different_brands
-
-    def set_fixed_price(self, product_category: str, key: str, value: Any) -> None:
-        """
-        Sets a fixed value of all keys for a specific category.
-        :param product_category: The category to change.
-        :param key: The key to find and update per each product of the category.
-        :param value: The value to set to all the keys.
-        """
-        query = {"categories": product_category}
-        new_values = {"$set": {key: value}}
-        self.collection.update_many(query, new_values)
-
-    def update_max_to_min(self, product_category: str, key: str) -> None:
-        """
-        Sets the minimum value of a key in a category to the maximum value of a key in the same category.
-        :param product_category: The category to swap in.
-        :param key: The key to swap between the two products.
-        """
-        min_doc_value = self.collection.find_one({"categories": product_category}, sort=[(key, 1)])[key]
-        max_doc = self.collection.find_one({"categories": product_category}, sort=[(key, -1)])
-        max_update = {"$set": {key: min_doc_value}}
-        self.collection.update_one(max_doc, max_update)
-
-    def change_value_for_all(self, key: str, difference: int) -> None:
-        """
-        Updates the value to all the products in the collection.
-        :param key: The key to add or sub from. Must hold a numeric value.
-        :param difference: The amount to add or sub.
-        """
-        change_update = {"$inc": {key: difference}}
-        self.collection.update_many({}, change_update)
+        pipeline = [
+            {"$unwind": "$sizes"},
+            {"$group": {"_id": "$sizes.size", "total": {"$sum": "$sizes.amount"}}}
+        ]
+        return self.collection.aggregate(pipeline)
